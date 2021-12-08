1ff0c1f2115223e8ef4a2077b28f6171e4517fcc
Nrhhmzh <69445191+Nrhhmzh@users.noreply.github.com>
2021-12-09 01:38:47 +0800

    Delete layout directory

--- layout/footer.php
+++
@@ -1,9 +0,0 @@
-    <!-- footer section start -->
-    <footer>
-        <span>Created By <a href="#">Naurah </a><span class="far fa-copyright"></span> 2021 All right reserved</span>
-    </footer>
-    <!-- footer section end -->
-
-    <script src="script.js"></script>
-</body>
-</html>
